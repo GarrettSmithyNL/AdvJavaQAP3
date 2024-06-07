102720a84d081e87699eba295ce37dd1b890c86d
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-06-07 13:06:19 -0230

    Started problem 1

---
+++ Person.java
@@ -0,0 +1,19 @@
+class Person {
+
+  protected String myName ; // name of the person 
+  protected int myAge; // person’s age 
+  protected String myGender; // “M” for male, “F” for female
+
+  public Person(String name, int age, String gender) {
+    myName = name; myAge = age ; myGender = gender;
+  }
+
+  // Getters
+
+  // Setter
+  
+
+  public String toString() {
+    return myName + ", age: " + myAge + ", gender: " +myGender; 
+  } 
+}
